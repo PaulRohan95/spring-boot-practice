fed18af0182431be53ca1ccb9195bf61ec74a227
Rohan Paul <paul.rohan08@gmail.com>
2026-03-29 22:08:16 +0530

    Added DTO layer

--- src/main/java/com/rohan/practice1/dto/TaskRequestDTO.java
+++ src/main/java/com/rohan/practice1/dto/TaskRequestDTO.java
@@ -1,4 +1,21 @@
 package com.rohan.practice1.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+
 public class TaskRequestDTO {
+    @NotBlank (message = "Title cannot be blank")
+    @Size (min = 2, max = 75, message = "Title should be between 2 and 75 characters")
+    private String title;
+
+    @NotBlank (message = "Description cannot be blank")
+    @Size (min = 5, max = 500, message = "Description should be between 5 and 500 characters")
+    private String description;
 }
--- src/main/java/com/rohan/practice1/dto/TaskResponseDTO.java
+++ src/main/java/com/rohan/practice1/dto/TaskResponseDTO.java
@@ -1,4 +1,13 @@
 package com.rohan.practice1.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class TaskResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
 }
